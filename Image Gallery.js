b5cfe04c5405e7bf6e8e456f8920da6ab0318fe4
Anshu2182005 <anshusahu2182005@gmail.com>
2025-06-30 17:02:45 +0530

    commit

--- Image Gallery.js
+++ Image Gallery.js
@@ -7,19 +7,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Sample image data with categories
     const images = [
-<<<<<<< HEAD
-        { src: 'img/nature 1.jpg', category: 'nature' },
-        { src: 'img/city1.jpg', category: 'city' },
-        { src: 'img/abs1.jpg', category: 'abstract' },
-        { src: 'img/nature 2.jpg', category: 'nature' },
-        { src: 'img/city2.jpg', category: 'city' },
-        { src: 'img/abs2.jpg', category: 'abstract' },
-        { src: 'img/nature 3.jpg', category: 'nature' },
-        { src: 'img/city3.jpg', category: 'city' },
-        { src: 'img/abs3.jpg', category: 'abstract' },
-        { src: 'img/nature 4.jpg', category: 'nature' },
-        { src: 'img/city4.jpg', category: 'city' },
-        { src: 'img/abs4.jpg', category: 'abstract' },
+        { src: './img/nature 1.jpg', category: 'nature' },
+        { src: './img/city1.jpg', category: 'city' },
+        { src: './img/abs1.jpg', category: 'abstract' },
+        { src: './img/nature 2.jpg', category: 'nature' },
+        { src: './img/city2.jpg', category: 'city' },
+        { src: './img/abs2.jpg', category: 'abstract' },
+        { src: './img/nature 3.jpg', category: 'nature' },
+        { src: './img/city3.jpg', category: 'city' },
+        { src: './img/abs3.jpg', category: 'abstract' },
+        { src: './img/nature 4.jpg', category: 'nature' },
+        { src: './img/city4.jpg', category: 'city' },
+        { src: './img/abs4.jpg', category: 'abstract' },
     ];
 
     let currentSlideIndex = 0;
